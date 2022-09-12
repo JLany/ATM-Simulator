d18544bc19910aa6c1848684a7bec6390489498f
Kilany <y.m.elkilany@gmail.com>
2022-09-12 09:52:47 +0300

    Implement Class Screen

--- ATM1.0/ATM1.0/Screen.cpp
+++ ATM1.0/ATM1.0/Screen.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
 #include "Screen.h"
+using namespace std;
+
+
+void displayMessage() {}
+template <class T, class... Types>
+void Screen::displayMessage(const T& arg, const Types&... args) const {
+	cout << fixed << setprecision(2); // set output formatting
+	cout << arg;
+
+	displayMessage(args...);
+}
+
+void displayMessageLine() { cout << "\n"; }
+template <class T, class... Types>
+void Screen::displayMessageLine(const T& arg, const Types&... args) const {
+	cout << fixed << setprecision(2); // set output formatting
+	cout << arg;
+
+	displayMessageLine(args...);
+}
