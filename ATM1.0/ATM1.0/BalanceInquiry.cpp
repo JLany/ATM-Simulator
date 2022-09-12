b9bf27413476f4f81103ca6f2e6ed8697dd862f6
Kilany <y.m.elkilany@gmail.com>
2022-09-12 09:29:43 +0300

    Implement Class BalanceInquiry

--- ATM1.0/ATM1.0/BalanceInquiry.cpp
+++ ATM1.0/ATM1.0/BalanceInquiry.cpp
@@ -1,1 +1,24 @@
 #include "BalanceInquiry.h"
+#include "Screen.h"
+#include "BankDatabase.h"
+using namespace std;
+
+BalanceInquiry::BalanceInquiry(int userAccountNumber,
+	Screen* aScreen,
+	BankDatabase* database)
+	: Transaction(userAccountNumber, aScreen, database) { // empty body
+} // end BalanceInquiry constructor
+
+void BalanceInquiry::execute() {
+	Screen* screen = getScreen();
+	BankDatabase* bankDatabase = getBankDatabase();
+
+	double availableBalance =
+		bankDatabase->getAvailableBalance(getAccountNumber());
+	double totalBalance =
+		bankDatabase->getTotalBalance(getAccountNumber());
+
+	screen->displayMessageLine("\nBalance Information:");
+	screen->displayMessageLine("	- Available Balance: $", availableBalance);
+	screen->displayMessageLine("	- Total Balance: $", totalBalance);
+}
