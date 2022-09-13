adc361688a5f32a991a4bae468ba7281fffdd8e2
Kilany <y.m.elkilany@gmail.com>
2022-09-13 17:55:02 +0300

    Update Class Screen Implementation

--- ATM1.0/ATM1.0/Screen.cpp
+++ ATM1.0/ATM1.0/Screen.cpp
@@ -1,35 +1,13 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
+#include "Screen.h"
 #include <chrono>
 #include <thread>
-#include "Screen.h"
 using namespace std;
 
-
-void displayMessage() {}
-template <class T, class... Types>
-void Screen::displayMessage(const T& arg, const Types&... args) const {
-	cout << fixed << setprecision(2); // set output formatting
-	cout << arg;
-
-	displayMessage(args...);
-}
-
-void displayMessageLine() { cout << "\n"; }
-template <class T, class... Types>
-void Screen::displayMessageLine(const T& arg, const Types&... args) const {
-	cout << fixed << setprecision(2); // set output formatting
-	cout << arg;
-
-	displayMessageLine(args...);
-}
+void Screen::clear() const {
+	system("cls");
+} // end function clear
 
 void Screen::hold(int aDuration) const {
-	chrono::seconds duration(aDuration);
-	this_thread::sleep_for(duration);
+	chrono::seconds sleepDuration(aDuration);
+	this_thread::sleep_for(sleepDuration);
 } // end function hold
-
-void Screen::clear() const {
-	system("clear");
-} // end function clear
--- ATM1.0/ATM1.0/Screen.h
+++ ATM1.0/ATM1.0/Screen.h
@@ -1,18 +1,30 @@
 #ifndef SCREEN_H
 #define SCREEN_H
 
-#include <string>
+#include <iostream>
+#include <iomanip>
+
 
 class Screen {
 public:
-	template <class T, class... Types>
-	void displayMessage(const T&, const Types&...) const;
+	void displayMessage() const { /* empty body */ } // base function for template
+	template<typename T, typename... Types>
+	void displayMessage(const T& arg, const Types&... args) const {
+		std::cout << std::fixed << std::setprecision(2); // set output formatting
+		std::cout << arg;
+		displayMessage(args...);
+	} // end function template displayMessage
 
-	template <class T, class... Types>
-	void displayMessageLine(const T&, const Types&...) const;
+	void displayMessageLine() const { std::cout << "\n"; } // base function for template
+	template<typename T, typename... Types>
+	void displayMessageLine(const T& arg, const Types&... args) const {
+		std::cout << std::fixed << std::setprecision(2); // set output formatting
+		std::cout << arg;
+		displayMessageLine(args...);
+	} // end function template displayMessageLine
 
-	void hold(int) const;
 	void clear() const;
+	void hold(int) const;
 }; // end class Screen
 
 #endif // SCREEN_H
