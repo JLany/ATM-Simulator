5a85006a2510928098d5ef33d75f2f1c624db0ad
Kilany <y.m.elkilany@gmail.com>
2022-09-14 13:26:10 +0300

    Remove Unnecessary #includes

--- ATM1.0/ATM1.0/main.cpp
+++ ATM1.0/ATM1.0/main.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <stdexcept>
 #include "ATM.h"
 #include "BankDatabase.h"
-using namespace std;
 
 int main() {
 	BankDatabase database;
